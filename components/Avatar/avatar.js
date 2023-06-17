a46c9d5d9dd559558e06f93f5fbdb6e40c2cd664
phoenixsparrow <abithjvinith@gmail.com>
2023-06-17 15:50:13 +0530

    profile section

--- components/Avatar/avatar.js
+++ components/Avatar/avatar.js
@@ -1,13 +1,71 @@
+// import { useState } from "react";
+// import { Avatar, Menu, MenuItem } from "@mui/material";
+// import { useDispatch, useSelector } from "react-redux";
+// // import { logout, selectUser } from "../redux/userSlice";
+
+// const AnimatedAvatar = () => {
+//   const [anchorEl, setAnchorEl] = useState(null);
+//   const dispatch = useDispatch();
+//   const user = useSelector((state) => state.auth.user);
+ 
+//   const handleMenuOpen = (event) => {
+//     setAnchorEl(event.currentTarget);
+//   };
+
+//   const handleMenuClose = () => {
+//     setAnchorEl(null);
+//   };
+
+//   const handleLogout = () => {
+//     // dispatch(logout());
+//     handleMenuClose();
+//   };
+
+//   const handleProfile = () => {
+//     // profile_user
+//     handleMenuClose();
+//   };
+
+//   console.log(user.avatar)
+//   return (
+//     <>
+//       <Avatar
+//         alt={user?.displayName}
+//         src={user.avatar}
+//         onClick={handleMenuOpen}
+//       />
+//       <Menu
+//         anchorEl={anchorEl}
+//         open={Boolean(anchorEl)}
+//         onClose={handleMenuClose}
+//         anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
+//         transformOrigin={{ vertical: "top", horizontal: "right" }}
+//       >
+
+        
+//         <MenuItem onClick={handleProfile}>Profile</MenuItem>
+//         <MenuItem onClick={handleLogout}>Logout</MenuItem>
+        
+//       </Menu>
+//     </>
+//   );
+// };
+
+// export default AnimatedAvatar;
+
+
+import { useRouter } from 'next/router';
 import { useState } from "react";
 import { Avatar, Menu, MenuItem } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
-// import { logout, selectUser } from "../redux/userSlice";
+// ...
 
 const AnimatedAvatar = () => {
   const [anchorEl, setAnchorEl] = useState(null);
   const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
- 
+  const router = useRouter();
+
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
   };
@@ -21,8 +79,10 @@ const AnimatedAvatar = () => {
     handleMenuClose();
   };
 
+
+    //profile datas
   const handleProfile = () => {
-    // profile_user
+    router.push('/profile');
     handleMenuClose();
   };
 
@@ -41,10 +101,9 @@ const AnimatedAvatar = () => {
         anchorOrigin={{ vertical: "bottom", horizontal: "right" }}
         transformOrigin={{ vertical: "top", horizontal: "right" }}
       >
-
-        
         <MenuItem onClick={handleProfile}>Profile</MenuItem>
         <MenuItem onClick={handleLogout}>Logout</MenuItem>
+
       </Menu>
     </>
   );
---
+++ pages/profile.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+const Profile = () => {
+  const user = useSelector((state) => state.auth.user);
+
+  useEffect(() => {
+    if (!user) {
+      // Redirect to login page if user is not logged in
+      window.location.href = '/login';
+    }
+  }, [user]);
+
+  const renderProfileContent = () => {
+    if (!user) {
+      return null;
+    }
+
+    const { role } = user;
+
+    if (role === 'student') {
+      return (
+        <div>
+          <h1>Student Profile</h1>
+          {/*student profile contents can be added here */}
+        </div>
+      );
+    } else if (role === 'educator') {
+      return (
+        <div>
+          <h1>Educator Profile</h1>
+          {/*educator profile content can be placed here*/}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>Invalid Role</h1>
+          {/* Handle invalid role scenario */}
+        </div>
+      );
+    }
+  };
+
+  return (
+    <div>
+      {renderProfileContent()}
+    </div>
+  );
+};
+
+export default Profile;
